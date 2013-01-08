c8491c50a3808f41385ea25ea9448424fcfa0e5b
Kristian Lyngstol <kristian@bohemians.org>
2013-01-08 19:04:38 +0100

    html/agent.js: add asserts, fix style

--- html/agent.js
+++ html/agent.js
@@ -1,140 +1,170 @@
+var stats = new Array();
+var paramlist;
+var out;
+var setstate = false;
 
-	var out;
-    	function vcl_load(vcl, id) {
-		var client = new XMLHttpRequest();
-		try {
+function assert(val)
+{
+	if (!(val)) {
+		throw "Assertion failure";
+	}
+}
+
+function assertText(val)
+{
+	assert(val != null);
+	assert(val.length > 0);
+}
+
+function vcl_load(vcl, id) {
+	var client = new XMLHttpRequest();
+	assertText(vcl);
+	assertText(id);
+	try {
 		client.open("PUT", "/vcl/" + id, false);
 		client.send(vcl);
 		out = client.responseText;
-		} catch (err) {
-			out = "Com. errors with agent: \n" + err;
-		}
-		if (client.status == 201) {
-			return true;
-		} else {
-			return false;
-		}
+	} catch (err) {
+		out = "Com. errors with agent: \n" + err;
 	}
-	function out_clear() {
-		document.getElementById("out").innerHTML = "";
-		out = "";
+	if (client.status == 201) {
+		return true;
+	} else {
+		return false;
 	}
-	function out_up() {
-		document.getElementById("out").innerHTML = out;
-	}
+}
 
-	function vcl_show(id) {
-		var client = new XMLHttpRequest();
-		try {
+function out_clear() {
+	document.getElementById("out").innerHTML = "";
+	out = "";
+}
+
+function out_up() {
+	document.getElementById("out").innerHTML = out;
+}
+
+function vcl_show(id) {
+	var client = new XMLHttpRequest();
+	assertText(id);
+	try {
 		client.open("GET", "/vcl/" + id, false);
 		client.send();
-		} catch(err) {
-		out = "Com. errors with agent: \n" + err;
+	} catch (err) {
+		out = "Com. errors with agent: \n" + err.message;
 		return null;
-		}
-		if (client.status == 200) {
-			return client.responseText;
-		} else {
-			out = client.responseText;
-			return null;
-		}
 	}
-	function topActive(head) {
-		var navs = new Array('nav-home', 'nav-vcl', 'nav-param');
+	if (client.status == 200) {
+		return client.responseText;
+	} else {
+		out = client.responseText;
+		return null;
+	}
+}
 
-		for (x in navs) {
-			document.getElementById(navs[x]).className = "";
-		}
-		document.getElementById("nav-" + head).className = "active";
-	}
+function topActive(head) {
+	var navs = new Array('nav-home', 'nav-vcl', 'nav-param');
+	assertText(head);
 
-	function showVCL() {
-		document.getElementById("params").style.display = "NONE";
-		document.getElementById("home").style.display = "NONE";
-		document.getElementById("vcl").style.display = "block";
-		topActive("vcl");
+	for (x in navs) {
+		document.getElementById(navs[x]).className = "";
 	}
-	function showHome() {
-		document.getElementById("params").style.display = "NONE";
-		document.getElementById("home").style.display = "block";
-		document.getElementById("vcl").style.display = "NONE";
-		topActive("home");
-	}
-	function showParam() {
-		document.getElementById("vcl").style.display = "NONE";
-		document.getElementById("home").style.display = "NONE";
-		document.getElementById("params").style.display = "block";
-		list_params();
-		topActive("param");
-	}
+	document.getElementById("nav-" + head).className = "active";
+}
+
+function showVCL() {
+	document.getElementById("params").style.display = "NONE";
+	document.getElementById("home").style.display = "NONE";
+	document.getElementById("vcl").style.display = "block";
+	topActive("vcl");
+}
+function showHome() {
+	document.getElementById("params").style.display = "NONE";
+	document.getElementById("home").style.display = "block";
+	document.getElementById("vcl").style.display = "NONE";
+	topActive("home");
+}
+function showParam() {
+	document.getElementById("vcl").style.display = "NONE";
+	document.getElementById("home").style.display = "NONE";
+	document.getElementById("params").style.display = "block";
+	list_params();
+	topActive("param");
+}
 
 
-	var setstate = false;
-	function reset_status() {
-		but = document.getElementById("status-btn");
-		try {
+function reset_status()
+{
+	but = document.getElementById("status-btn");
+	assert(but != null);
+	try {
 		var client = new XMLHttpRequest();
 		client.open("GET", "/status", false);
 		client.send();
 		stat = client.responseText;
-		} catch (err) {
+		assertText(stat);
+	} catch (err) {
 		stat = "Error communicating with agent: " + err;
-		}
-		but.textContent = stat;
-		if (stat == "Child in state running") {
-			but.className = "btn btn-primary btn-block disabled";
-		} else {
-			but.className = "btn btn-danger btn-block disabled";
-		}
-		setstate = false;
-	}	
-	function show_status(state,message) {
-		setstate = true;
-		but = document.getElementById("status-btn");
-		if (state == "ok")
-			but.className = "btn btn-success btn-block disabled";
-		if (state == "warn")
-			but.className = "btn btn-warning btn-block disabled";
-		if (state == "error")
-			but.className = "btn btn-danger btn-block disabled";
-		but.textContent = message;
-		setTimeout(function() { reset_status(); }, 3000);
 	}
+	but.textContent = stat;
+	if (stat == "Child in state running") {
+		but.className = "btn btn-primary btn-block disabled";
+	} else {
+		but.className = "btn btn-danger btn-block disabled";
+	}
+	setstate = false;
+}	
 
-	function uploadVCL() {
-		out_clear();
-		var client = new XMLHttpRequest();
-		ret = vcl_load(document.getElementById("vcl-text").value, document.getElementById("vclID").value);
-		out_up();
-		if (ret) {
-			show_status("ok","VCL stored");
-		} else {
-			show_status("warn", "VCL save failed");
-		}
-	}
+function show_status(state,message)
+{
+	assertText(state);
+	assertText(message);
+	setstate = true;
+	but = document.getElementById("status-btn");
+	if (state == "ok")
+		but.className = "btn btn-success btn-block disabled";
+	if (state == "warn")
+		but.className = "btn btn-warning btn-block disabled";
+	if (state == "error")
+		but.className = "btn btn-danger btn-block disabled";
+	but.textContent = message;
+	setTimeout(function() { reset_status(); }, 3000);
+}
 
-	function loadVCL() {
-		out_clear();
-		ret = vcl_show(document.getElementById("loader").value);
-		if (ret == null)
-			show_status("warn","VCL load failed");
-		else
-			show_status("ok", "VCL loaded");
-		document.getElementById("vcl-text").value = ret;
-		document.getElementById("vclID").value = document.getElementById("loader").value;
-		out_up();
+function uploadVCL()
+{
+	out_clear();
+	ret = vcl_load(document.getElementById("vcl-text").value, document.getElementById("vclID").value);
+	out_up();
+	if (ret) {
+		show_status("ok","VCL stored");
+	} else {
+		show_status("warn", "VCL save failed");
 	}
-	function listVCL() {
-		var client = new XMLHttpRequest();
-		client.open("GET", "/vcljson/", false);
-		try {
+}
+
+function loadVCL()
+{
+	out_clear();
+	ret = vcl_show(document.getElementById("loader").value);
+	if (ret == null)
+		show_status("warn","VCL load failed");
+	else
+		show_status("ok", "VCL loaded");
+	document.getElementById("vcl-text").value = ret;
+	document.getElementById("vclID").value = document.getElementById("loader").value;
+	out_up();
+}
+
+function listVCL() {
+	var client = new XMLHttpRequest();
+	client.open("GET", "/vcljson/", false);
+	try {
 		client.send();
 		var vcllist = JSON.parse(client.responseText);
 		if (client.responseText == vcl)
 			return;
 		vcl = client.responseText;
 		var txt = "";
-		var d = document.getElementById("loader");
 		var active = "";
 		for (x in vcllist["vcls"]) {
 			v = vcllist["vcls"][x];
@@ -154,21 +184,20 @@
 			document.getElementById("vclID").value = active;
 			loadVCL();
 		}
-		} catch (err) {
-			out = "... failed.";
-		}
+	} catch (err) {
+		out = "Listing VCL failed.";
 	}
+}
 
-	var paramlist;
-
-	function list_params() {
-		try {
+function list_params()
+{
+	try {
 		var client = new XMLHttpRequest();
 		client.open("GET", "/paramjson/", false);
 		client.send();
 		if (client.status != 200)
 			throw new Error("Varnish-Agent returned " +
-			client.status + ":" + client.responseText);
+					client.status + ":" + client.responseText);
 		paramlist = JSON.parse(client.responseText);
 		var list = document.getElementById("param-sel");
 		var v = list.value;
@@ -184,165 +213,192 @@
 		if (v)
 			list.value = v;
 		paramChange();
-		} catch (err) {
-			out_clear();
-			out = "Error listing parameters.\n"
+	} catch (err) {
+		out_clear();
+		out = "Error listing parameters.\n"
 			out = out + "Communication error with agent: \n"
 			out = out + err;
-			out_up();
-		}
-
-		return paramlist;
-	}
-
-	function paramChange() {
-		var pname = document.getElementById("param-sel").value;
-		document.getElementById("param-val").value = paramlist[pname].value;
-		out_clear();
-		out = "Default: " + paramlist[pname].default + "\n";
-		out = out + "Value:   " + paramlist[pname].value + "\n";
-		out = out + "Unit:    " + paramlist[pname].unit + "\n";
-		out = out + paramlist[pname].description;
 		out_up();
 	}
-	function paramListDiff() {
-		out_clear();
-		out = "Non-default parameters:\n\n"
-		for (x in paramlist) {
-			if (paramlist[x].unit == "seconds" || paramlist[x].unit == "s" || paramlist[x].unit == "bitmap") {
-				one = Number(paramlist[x].value);
-				two = Number(paramlist[x].default);
-				if (one != two) {
-					out = out + x + "(";
-					out = out + one + " vs ";
-					out = out + two + ")\n";
-				}
-			} else if (paramlist[x].unit == "bytes") {
-				one = paramlist[x].value == "-1" ? "unlimited" : paramlist[x].value;
-				two = paramlist[x].default == "-1" ? "unlimited" : paramlist[x].default;
-				if (one != two) {
-					out = out + x + "(";
-					out = out + one + " vs ";
-					out = out + two + ")\n";
-				}
+	return paramlist;
+}
 
-			} else if (paramlist[x].value != paramlist[x].default) {
+function paramChange()
+{
+	var pname = document.getElementById("param-sel").value;
+	assertText(pname);
+	document.getElementById("param-val").value = paramlist[pname].value;
+	out_clear();
+	out = "Default: " + paramlist[pname].default + "\n";
+	out = out + "Value:   " + paramlist[pname].value + "\n";
+	out = out + "Unit:    " + paramlist[pname].unit + "\n";
+	out = out + paramlist[pname].description;
+	out_up();
+}
+
+function paramListDiff()
+{
+	out_clear();
+	out = "Non-default parameters:\n\n";
+	assert(paramlist != null);
+	for (x in paramlist) {
+		if (paramlist[x].unit == "seconds" || paramlist[x].unit == "s" || paramlist[x].unit == "bitmap") {
+			one = Number(paramlist[x].value);
+			two = Number(paramlist[x].default);
+			if (one != two) {
 				out = out + x + "(";
-				out = out + paramlist[x].value + " vs ";
-				out = out + paramlist[x].default + ")\n";
+				out = out + one + " vs ";
+				out = out + two + ")\n";
 			}
-		}
-		out_up();
-	}
-	function saveParam()
-	{
-		var pname = document.getElementById("param-sel").value;
-		var pval = document.getElementById("param-val").value;
-		var client = new XMLHttpRequest();
-		out_clear();
-		client.open("PUT", "/param/" + pname, false);
-		client.send(pval);
-		out = client.responseText;
-		out_up();
-		if (client.status == 200) {
-			show_status("ok","Parameter saved");
-			list_params();
-		} else {
-			show_status("warn","Couldn't save parameter");
+		} else if (paramlist[x].unit == "bytes") {
+			one = paramlist[x].value == "-1" ? "unlimited" : paramlist[x].value;
+			two = paramlist[x].default == "-1" ? "unlimited" : paramlist[x].default;
+			if (one != two) {
+				out = out + x + "(";
+				out = out + one + " vs ";
+				out = out + two + ")\n";
+			}
+
+		} else if (paramlist[x].value != paramlist[x].default) {
+			out = out + x + "(";
+			out = out + paramlist[x].value + " vs ";
+			out = out + paramlist[x].default + ")\n";
 		}
 	}
-	function setParamDef()
-	{
-		var pname = document.getElementById("param-sel").value;
-		var pval = document.getElementById("param-val");
-		pval.value = paramlist[pname].default;
+	out_up();
+}
+
+function saveParam()
+{
+	var pname = document.getElementById("param-sel").value;
+	var pval = document.getElementById("param-val").value;
+	var client = new XMLHttpRequest();
+	assertText(pname);
+	assertText(pval);
+	out_clear();
+	client.open("PUT", "/param/" + pname, false);
+	client.send(pval);
+	out = client.responseText;
+	out_up();
+	if (client.status == 200) {
+		show_status("ok","Parameter saved");
+		list_params();
+	} else {
+		show_status("warn","Couldn't save parameter");
 	}
-	function vcl_use(id) {
-		var client = new XMLHttpRequest();
-		try {
+}
+
+function setParamDef()
+{
+	var pname = document.getElementById("param-sel").value;
+	var pval = document.getElementById("param-val");
+	assertText(pname);
+	pval.value = paramlist[pname].default;
+}
+
+function vcl_use(id)
+{
+	var client = new XMLHttpRequest();
+	assertText(id);
+	try {
 		client.open("PUT", "/vcldeploy/" + id, false);
 		client.send();
 		out = client.responseText;
-		} catch (err) {
+	} catch (err) {
 		out = "Com. errors with agent: \n" + err;
 		return false;
-		}
-		if (client.status = "200") {
-			return true;
-		} else {
-			return false;
-		}
 	}
-	function deployVCL() {
-		out_clear();
-		id = document.getElementById("loader").value;
-		doc = vcl_use(id);
-		if (doc) {
-			show_status("ok","VCL deployed");
-		} else {
-			show_status("warn","vcl deploy failed");
-		}
-		out_up();	
+	if (client.status = "200") {
+		return true;
+	} else {
+		return false;
 	}
-	function clearID() {
-		document.getElementById("vclID").value = "";
+}
+
+function deployVCL()
+{
+	out_clear();
+	id = document.getElementById("loader").value;
+	assertText(id);
+	doc = vcl_use(id);
+	if (doc) {
+		show_status("ok","VCL deployed");
+	} else {
+		show_status("warn","vcl deploy failed");
 	}
-	function vcl_discard(id) {
-		try {
+	out_up();	
+}
+
+function clearID()
+{
+	document.getElementById("vclID").value = "";
+}
+
+function vcl_discard(id)
+{
+	assertText(id);
+	try {
 		var client = new XMLHttpRequest();
 		client.open("DELETE", "/vcl/" + id, false);
 		client.send();
 		out = client.responseText;
-		} catch (err) {
+	} catch (err) {
 		out = "Com. errors with agent: \n" + err;
 		return false;
-		}
-		if (client.status = "200" && client.responseText == "") {
-			return true;
-		} else {
-			return false;
-		}
 	}
-	function discardVCL() {
-		out_clear();
-		doc = vcl_discard(document.getElementById("loader").value);
-		if (doc) {
-			show_status("ok","VCL discarded");
-		} else {
-			show_status("warn","VCL discard failed");
-		}
-		out_up();	
+	if (client.status = "200" && client.responseText == "") {
+		return true;
+	} else {
+		return false;
 	}
-	function stop() {
-		var client = new XMLHttpRequest();
-		client.open("PUT", "/stop", false);
-		client.send();
-		doc = client.responseText;
-		document.getElementById("out").innerHTML = doc;
-		status();
+}
+
+function discardVCL()
+{
+	out_clear();
+	doc = vcl_discard(document.getElementById("loader").value);
+	if (doc) {
+		show_status("ok","VCL discarded");
+	} else {
+		show_status("warn","VCL discard failed");
 	}
-	function start() {
-		var client = new XMLHttpRequest();
-		client.open("PUT", "/start", false);
-		client.send();
-		doc = client.responseText;
-		document.getElementById("out").innerHTML = doc;
-		status();
-	}
-	function status() {
-		var client = new XMLHttpRequest();
-		try {
-		listVCL();
-		} catch (err) {
-		}
-		if (!setstate)
-			reset_status();
-	}
-	var stats = new Array();
-	function update_stats() {
-		var client = new XMLHttpRequest();
-		var d = document.getElementById("stats-btn");
-		try {
+	out_up();	
+}
+
+function stop()
+{
+	var client = new XMLHttpRequest();
+	client.open("PUT", "/stop", false);
+	client.send();
+	doc = client.responseText;
+	document.getElementById("out").innerHTML = doc;
+	status();
+}
+
+function start()
+{
+	var client = new XMLHttpRequest();
+	client.open("PUT", "/start", false);
+	client.send();
+	doc = client.responseText;
+	document.getElementById("out").innerHTML = doc;
+	status();
+}
+
+function status()
+{
+	var client = new XMLHttpRequest();
+	listVCL();
+	if (!setstate)
+		reset_status();
+}
+
+function update_stats()
+{
+	var client = new XMLHttpRequest();
+	var d = document.getElementById("stats-btn");
+	assert(d != null);
+	try {
 		client.open("GET","/stats", false);
 		client.send();
 		for (i = 0; i < 3; i++) {
@@ -364,12 +420,12 @@
 		}
 		if (n_n_req>0)
 			d.innerHTML = Number(n_req/n_n_req).toFixed(0) + "req/s\n";
-		} catch (err) {
-			d.innerHTML = "Couldn't get stats";
-		}
-
+	} catch (err) {
+		d.innerHTML = "Couldn't get stats: " + err;
 	}
-	$('.btn').button();
-	setInterval(function(){status()},10000);
-	setInterval(function(){update_stats()},1000);
-	listVCL();
+
+}
+$('.btn').button();
+setInterval(function(){status()},10000);
+setInterval(function(){update_stats()},1000);
+listVCL();
