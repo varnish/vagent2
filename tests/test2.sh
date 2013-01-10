1118ff39e5535c5df6ddaf4a2a265e368b28721c
Kristian Lyngstol <kristian@bohemians.org>
2013-01-10 08:30:46 +0100

    Move test data and tmp files

--- tests/test2.sh
+++ tests/test2.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 N=1
+if [ "$(basename $PWD)" != "tests" ]; then
+	echo "Must run tests from tests/ directory";
+	exit 1;
+fi
 function test_vcl()
 {
+	OUT=tmp/$(basename $VCL1).tmp
 	lwp-request -m PUT http://localhost:6085/vcl/test3 <$VCL1 > /dev/null
 	if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; echo $FOO; fi
 	inc
-	lwp-request -m GET http://localhost:6085/vcl/test3 >$VCL1.tmp
+	lwp-request -m GET http://localhost:6085/vcl/test3 >$OUT
 	if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; fi
 	inc
-	if diff -q $VCL1 $VCL1.tmp > /dev/null; then echo "Passed ${N}"; else echo "Failed ${N}"; fi
+	if diff -q $VCL1 $OUT > /dev/null; then echo "Passed ${N}"; else echo "Failed ${N}"; fi
 	inc
 	FOO=$(lwp-request -m DELETE http://localhost:6085/vcl/test3 || exit 1)
 	if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; fi
@@ -30,7 +35,7 @@ inc
 FOO=$(lwp-request -m GET http://localhost:6085/vcl/boot || exit 1)
 if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; fi
 inc
-VCL1=smallvcl
+VCL1=data/smallvcl
 test_vcl
-VCL1=longvcl
+VCL1=data/longvcl
 test_vcl
