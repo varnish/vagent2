b24b3324c00b04bf60e35af42597827f18b4092f
Kristian Lyngstol <kristian@bohemians.org>
2013-01-17 09:41:31 +0100

    tests/ban.sh: Test bans

---
+++ tests/ban.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$(basename $PWD)" != "tests" ]; then
+	echo "Must run tests from tests/ directory";
+	exit 1;
+fi
+. util.sh
+
+is_running
+test_it_long GET ban "" "Present bans"
+GET http://localhost/meh > /dev/null
+sleep 1
+test_it_long_content_fail GET ban "" "/meh"
+test_it POST ban "req.url ~ /meh" ""
+test_it_long GET ban "" "/meh"
+GET http://localhost/meh > /dev/null
+sleep 1
+test_it_long_content_fail GET ban "" "/meh"
+test_it_no_content POST ban/meh ""
+test_it_long GET ban "" "/meh"
+GET http://localhost/meh > /dev/null
+sleep 1
+test_it_long_content_fail GET ban "" "/meh"
--- tests/util.sh
+++ tests/util.sh
@@ -17,6 +17,15 @@ function test_it()
 	inc
 }
 
+function test_it_no_content()
+{
+	FOO=$(lwp-request -m $1 http://localhost:6085/$2 </dev/null)
+	if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; echo $FOO; fi
+	inc
+	if [ "x$FOO" = "x$4" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; echo $FOO; fi
+	inc
+}
+
 function test_it_fail()
 {
 	FOO=$(lwp-request -m $1 http://localhost:6085/$2 <<<"$3")
@@ -32,6 +41,15 @@ function test_it_long()
 	if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; echo $FOO; fi
 	inc
 	if echo $FOO | grep -q "$4"; then echo "Passed ${N}"; else echo "Failed ${N}"; echo $FOO; fi
+	inc
+}
+
+function test_it_long_content_fail()
+{
+	FOO=$(lwp-request -m $1 http://localhost:6085/$2 <<<"$3")
+	if [ "x$?" = "x0" ]; then echo "Passed ${N}"; else echo "Failed ${N}"; echo $FOO; fi
+	inc
+	if echo $FOO | grep -q "$4"; then echo "Failed ${N}"; echo $FOO; else echo "Passed ${N}"; fi
 	inc
 }
 
