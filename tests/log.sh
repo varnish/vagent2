c8e6ef1c043432302260f1cfaf37a672207d652c
Kristian Lyngstol <kristian@bohemians.org>
2013-01-17 09:22:13 +0100

    tests/log.sh added

---
+++ tests/log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$(basename $PWD)" != "tests" ]; then
+	echo "Must run tests from tests/ directory";
+	exit 1;
+fi
+. util.sh
+
+is_running
+test_it_long GET log "" "\"tag\":"
+test_it_fail GET log/0 "" "Not a number"
+test_it_long GET log/2 "" "\"tag\":"
+GET http://localhost/foobar > /dev/null
+test_it_long GET log/1/RxURL "" "\"tag\":"
+test_it_long GET log/1/RxURL/foobar "" "/foobar"
