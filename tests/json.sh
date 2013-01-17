5c827699e38f0713a0b59184c3f3251e50222767
Kristian Lyngstol <kristian@bohemians.org>
2013-01-17 11:02:16 +0100

    tests/json: Demonstrate CLI-vlog breakage
    References #43

--- tests/json.sh
+++ tests/json.sh
@@ -17,4 +17,10 @@ function test_json()
 test_json vcljson/
 test_json paramjson/
 test_json stats
-test_json log
+test_json log/100/RxURL
+
+# XXX: This will produce a CLI command for vcl.list, and a log entry
+# thereafter.
+# This is known to break, but should still be a test case.
+test_it_long GET vcl/ "" "active"
+test_json log/100/CLI
