51cfd41618fcf4acd7913a1957f8e2ddee46dd85
Kristian Lyngstol <kristian@bohemians.org>
2013-01-30 13:57:18 +0100

    tests: Reduce ban timers

--- tests/ban.sh
+++ tests/ban.sh
@@ -10,16 +10,16 @@ init_all
 is_running
 test_it_long GET ban "" ".*"
 GET http://localhost:${VARNISH_PORT}/meh > /dev/null
-sleep 1
+sleep 0.5
 test_it_long_content_fail GET ban "" "/meh"
 test_it POST ban "req.url ~ /meh" ""
 test_it_long GET ban "" "/meh"
 GET http://localhost:${VARNISH_PORT}/meh > /dev/null
-sleep 1
+sleep 0.5
 test_it_long_content_fail GET ban "" "/meh"
 test_it_no_content POST ban/meh ""
 test_it_long GET ban "" "/meh"
 GET http://localhost:${VARNISH_PORT}/meh > /dev/null
-sleep 1
+sleep 0.5
 test_it_long_content_fail GET ban "" "/meh"
 exit $ret
