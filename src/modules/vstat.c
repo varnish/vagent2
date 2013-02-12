f8a7f252115d85370b3522439caa1de002cdf4ce
Kristian Lyngstol <kristian@bohemians.org>
2013-02-12 16:22:11 +0100

    vstat: Introduce locking, preparing for timer
    Next step: Periodic POST of stats.

--- src/modules/vstat.c
+++ src/modules/vstat.c
@@ -45,6 +45,7 @@
 #include <varnishapi.h>
 #include <time.h>
 #include <vsc.h>
+#include <pthread.h>
 #include "vsb.h"
 #include "common.h"
 #include "httpd.h"
@@ -58,51 +59,53 @@ struct vstat_priv_t {
 	int jp;
 	int open;
 	const struct VSC_C_main *VSC_C_main;
-	struct vsb *vsb;
+	struct vsb *vsb_http;
+	struct vsb *vsb_timer;
 	time_t now;
 	int last_uptime;
+	pthread_mutex_t lck;
 };
 
 static int
 do_json_cb(void *priv, const struct VSC_point * const pt)
 {
 	uint64_t val;
-	struct vstat_priv_t *vstat = priv;
+	struct vsb *out_vsb = priv;
 	assert(!strcmp(pt->fmt, "uint64_t"));
 	val = *(const volatile uint64_t*)pt->ptr;
 	assert(sizeof(uint64_t) == sizeof(uintmax_t));
 
-	VSB_printf(vstat->vsb, ",\n\t\"");
+	VSB_printf(out_vsb, ",\n\t\"");
 	/* build the JSON key name.  */
 	if (pt->class[0])
-		VSB_printf(vstat->vsb,"%s.",pt->class);
+		VSB_printf(out_vsb,"%s.",pt->class);
 	if (pt->ident[0])
-		VSB_printf(vstat->vsb,"%s.",pt->ident);
-	VSB_printf(vstat->vsb,"%s\": {", pt->name);
+		VSB_printf(out_vsb,"%s.",pt->ident);
+	VSB_printf(out_vsb,"%s\": {", pt->name);
 
 	if (strcmp(pt->class, ""))
-		VSB_printf(vstat->vsb,"\"type\": \"%s\", ", pt->class);
+		VSB_printf(out_vsb,"\"type\": \"%s\", ", pt->class);
 	if (strcmp(pt->ident, ""))
-		VSB_printf(vstat->vsb,"\"ident\": \"%s\", ", pt->ident);
+		VSB_printf(out_vsb,"\"ident\": \"%s\", ", pt->ident);
 
-	VSB_printf(vstat->vsb, "\"value\": %ju, ", (uintmax_t)val);
-	VSB_printf(vstat->vsb, "\"flag\": \"%c\",", pt->flag);
-	VSB_printf(vstat->vsb, "\"description\": \"%s\" }", pt->desc);
+	VSB_printf(out_vsb, "\"value\": %ju, ", (uintmax_t)val);
+	VSB_printf(out_vsb, "\"flag\": \"%c\",", pt->flag);
+	VSB_printf(out_vsb, "\"description\": \"%s\" }", pt->desc);
 	return (0);
 }
 
 static void
-do_json(struct vstat_priv_t *vstat)
+do_json(struct vstat_priv_t *vstat, struct vsb *out_vsb)
 {
 	char time_stamp[20];
 	time_t now;
 	now = time(NULL);
 
-	assert(vstat->vsb);
+	assert(out_vsb);
 	(void)strftime(time_stamp, 20, "%Y-%m-%dT%H:%M:%S", localtime(&now));
-	VSB_printf(vstat->vsb, "{\n\t\"timestamp\": \"%s\"", time_stamp);
-	(void)VSC_Iter(vstat->vd, do_json_cb, vstat);
-	VSB_printf(vstat->vsb, "\n}\n");
+	VSB_printf(out_vsb, "{\n\t\"timestamp\": \"%s\"", time_stamp);
+	(void)VSC_Iter(vstat->vd, do_json_cb, out_vsb);
+	VSB_printf(out_vsb, "\n}\n");
 }
 
 static void vstat_open(struct vstat_priv_t *vstat)
@@ -133,6 +136,7 @@ static unsigned int vstat_reply(struct httpd_request *request, void *data)
 {
 	struct vstat_priv_t *vstat;
 	GET_PRIV(data,vstat);
+	pthread_mutex_lock(&vstat->lck);
 	if (!vstat->open) {
 		vstat_open(vstat);
 	}
@@ -146,15 +150,17 @@ static unsigned int vstat_reply(struct httpd_request *request, void *data)
 	}
 
 	if (!vstat->open) {
+		pthread_mutex_unlock(&vstat->lck);
 		send_response_fail(request->connection, "Couldn't open shmlog");
 		return 0;
 	}
 
 
-	do_json(vstat);
-	assert(VSB_finish(vstat->vsb) == 0);
-	send_response(request->connection, 200, VSB_data(vstat->vsb), VSB_len(vstat->vsb));
-	VSB_clear(vstat->vsb);
+	do_json(vstat, vstat->vsb_http);
+	pthread_mutex_unlock(&vstat->lck);
+	assert(VSB_finish(vstat->vsb_http) == 0);
+	send_response(request->connection, 200, VSB_data(vstat->vsb_http), VSB_len(vstat->vsb_http));
+	VSB_clear(vstat->vsb_http);
 	return 0;
 }
 
@@ -172,9 +178,11 @@ vstat_init(struct agent_core_t *core)
 	assert(priv->vd);
 	VSC_Setup(priv->vd);
 	priv->jp = 0;
-	priv->vsb = NULL;
-	priv->vsb = VSB_new_auto();
+	priv->vsb_http = NULL;
+	priv->vsb_http = VSB_new_auto();
 	plug->data = priv;
+
+	pthread_mutex_init(&priv->lck, NULL);
 
 	if (core->config->n_arg)
 		VSC_Arg(priv->vd, 'n', core->config->n_arg);
