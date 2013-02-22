a9ea6667cd5371e073b4ead6ca2a96d3547ae73a
Kristian Lyngstol <kristian@bohemians.org>
2013-02-22 14:09:25 +0100

    tests/buffers: Output tweak

--- tests/buffers.sh
+++ tests/buffers.sh
@@ -7,6 +7,6 @@ fi
 . util.sh
 init_all
 
-test_it_long_file_fail PUT echo data/large "500 Server closed connection without sending any data back"
+test_it_long_file_fail PUT echo data/large "Server closed connection without sending any data back"
 test_it_long_file PUT echo data/1.3M "200"
 exit $ret
