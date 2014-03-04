048373c617ffcc663389eaa6e47435b1a6fa6ced
Dag Haavi Finstad <daghf@varnish-software.com>
2014-03-04 13:57:28 +0100

    Varnish4 takes a more lazy approach to cleaning up the ban lists, so we can't deterministically test for bans being gone by consulting ban.list any longer ..

--- tests/ban.sh
+++ tests/ban.sh
@@ -14,12 +14,6 @@ sleep 1
 test_it_long_content_fail GET ban "" "/meh"
 test_it POST ban "req.url ~ /meh" ""
 test_it_long GET ban "" "/meh"
-GET http://localhost:${VARNISH_PORT}/meh > /dev/null
-sleep 1
-test_it_long_content_fail GET ban "" "/meh"
 test_it_no_content POST ban/meh ""
 test_it_long GET ban "" "/meh"
-GET http://localhost:${VARNISH_PORT}/meh > /dev/null
-sleep 1
-test_it_long_content_fail GET ban "" "/meh"
 exit $ret
