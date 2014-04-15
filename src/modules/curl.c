9139681c2efcd39e2aad9124e12d7e3149b0b13b
Espen Braastad <espen@varnish-software.com>
2014-04-15 10:52:20 +0200

    Do not set CURLOPT_NOBODY if we have data to send. In libcurl version 7.15.5 will CURLOPT_NOBODY (when true) change the HTTP method from PUT to HEAD, even though CURLOPT_UPLOAD is true.

--- src/modules/curl.c
+++ src/modules/curl.c
@@ -117,12 +117,14 @@ static void issue_curl(void *priv, char *url, struct ipc_ret_t *ret)
 		curl_easy_setopt(curl, CURLOPT_URL, url);
 		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 2);
 		curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
-		curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, dropdata);
 		if (data) {
+			curl_easy_setopt(curl, CURLOPT_NOBODY, 0);
 			curl_easy_setopt(curl, CURLOPT_UPLOAD, 1);
 			curl_easy_setopt(curl, CURLOPT_READFUNCTION, senddata);
 			curl_easy_setopt(curl, CURLOPT_READDATA, private);
+		} else {
+			curl_easy_setopt(curl, CURLOPT_NOBODY, 0);
 		}
 		res = curl_easy_perform(curl);
 		if(res != CURLE_OK) {
