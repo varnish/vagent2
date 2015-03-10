6478f8068ee4840d13369c56375dc30da070cbf7
Federico G. Schwindt <fgsch@lodoss.net>
2015-03-10 09:57:50 +0000

    Indentation and cosmetic changes
    While here drop asprintf in favour of strdup.

--- src/modules/http.c
+++ src/modules/http.c
@@ -26,7 +26,6 @@
  * SUCH DAMAGE.
  */
 
-#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -42,8 +41,25 @@
 #include "http.h"
 #include "base64.h"
 
-
-#define RCV_BUFFER 2048000
+#define RCV_BUFFER	2 * 1000 * 1024
+#define HELP_TEXT							\
+	"This is the varnish agent.\n\n"				\
+	"GET requests never modify state\n"				\
+	"POST requests are not idempotent, and can modify state\n"	\
+	"PUT requests are idempotent, and can modify state\n"		\
+	"HEAD requests can be performed on all resources that support GET\n" \
+	"\nThe following URLs are bound:\n\n"
+#define SEND(name, status)						\
+int									\
+send_response_##name(struct MHD_Connection *conn, const char *data)	\
+{									\
+	struct http_response resp = {					\
+		conn, NULL, (status), data, strlen(data)		\
+	};								\
+	return (send_response(&resp));					\
+}
+SEND(ok, 200)
+SEND(fail, 500)
 
 struct http_listener {
 	char *url;
@@ -65,6 +81,11 @@ struct connection_info_struct {
 	int authed;
 };
 
+struct header_finder_t {
+	const char *header;
+	char *value;
+};
+
 struct http_content_type {
 	const char *file_ext;
 	const char *content_type;
@@ -79,65 +100,50 @@ struct http_content_type {
 	{ NULL,		NULL }
 };
 
-#define SEND(name, status)						\
-int									\
-send_response_##name(struct MHD_Connection *conn, const char *data)	\
-{									\
-	struct http_response resp = {					\
-		conn, NULL, (status), data, strlen(data)		\
-	};								\
-	return (send_response(&resp));					\
-}
-SEND(ok, 200)
-SEND(fail, 500)
-
-static char *make_help(struct http_priv_t *http)
+static char *
+make_help(struct http_priv_t *http)
 {
-	char *body;
-	char buffer[512];
-	struct http_listener *listener;
-	int ret;
-	ret = asprintf(&body,"This is the varnish agent.\n\n"
-		"GET requests never modify state\n"
-		"POST requests are not idempotent, and can modify state\n"
-		"PUT requests are idempotent, and can modify state\n"
-		"HEAD requests can be performed on all resources that support GET\n"
-		"\n"
-		"The following URLs are bound:\n\n"
-		);
-	assert(ret>0);
+	struct http_listener *lp;
+	char buf[512];
+	char *data;
 
-	for (listener = http->listener; listener != NULL; listener = listener->next) {
-		snprintf(buffer, 512, " - %-20s %-3s %-3s %-4s %s\n",
-			listener->url,
-			(listener->method & M_GET) ? "GET" : "",
-			(listener->method & M_PUT) ? "PUT" : "",
-			(listener->method & M_POST) ? "POST" : "",
-			(listener->method & M_DELETE) ? "DELETE" : ""
-			);
-		/*
-		 * \0 and newline at the end
-		 */
-		body = realloc(body, strlen(body) + strlen(buffer) + 2);
-		assert(body);
-		strcat(body,buffer);
+	data = strdup(HELP_TEXT);
+	assert(data);
+
+	for (lp = http->listener; lp != NULL; lp = lp->next) {
+		snprintf(buf, sizeof(buf), " - %-20s %-3s %-3s %-4s %s\n",
+		    lp->url,
+		    (lp->method & M_GET) ? "GET" : "",
+		    (lp->method & M_PUT) ? "PUT" : "",
+		    (lp->method & M_POST) ? "POST" : "",
+		    (lp->method & M_DELETE) ? "DELETE" : "");
+		/* \0 and newline at the end */
+		data = realloc(data, strlen(data) + strlen(buf) + 2);
+		assert(data);
+		strcat(data, buf);
 	}
-	strcat(body,"\n");
-	return body;
+	strcat(data, "\n");
+	return (data);
 }
 
-static int send_auth_response(struct MHD_Connection *connection)
+static void
+send_auth_response(struct MHD_Connection *connection)
 {
-	struct http_response *resp = http_mkresp(connection, 401, "Authorize, please.\n\n" \
-	    "If Varnish Agent was installed from packages, the /etc/varnish/agent_secret " \
-	    "file contains generated credentials.");
-	http_add_header(resp, "WWW-Authenticate", "Basic realm=varnish-agent");
+	struct http_response *resp;
+
+	resp = http_mkresp(connection, 401, "Authorize, please.\n\n"
+	    "If Varnish Agent was installed from packages, the "
+	    "/etc/varnish/agent_secret file contains generated "
+	    "credentials.");
+	http_add_header(resp, "WWW-Authenticate",
+	    "Basic realm=varnish-agent");
 	send_response(resp);
 	http_free_resp(resp);
-	return 1;
 }
 
-void http_add_header(struct http_response *resp, const char *key, const char *value)
+void
+http_add_header(struct http_response *resp, const char *key,
+    const char *value)
 {
 	struct http_header *hdr;
 
@@ -153,22 +159,23 @@ void http_add_header(struct http_response *resp, const char *key, const char *va
 	resp->headers = hdr;
 }
 
-
-void http_free_resp(struct http_response *resp)
+void
+http_free_resp(struct http_response *resp)
 {
-	struct http_header *hdr;
-	struct http_header *to_delete;
-	for (hdr = resp->headers; hdr; ) {
+	struct http_header *hdr, *ohdr;
+
+	for (hdr = resp->headers; hdr;) {
 		free(hdr->key);
 		free(hdr->value);
-		to_delete = hdr;
+		ohdr = hdr;
 		hdr = hdr->next;
-		free(to_delete);
+		free(ohdr);
 	}
 	free(resp);
 }
 
-struct http_response *http_mkresp(struct MHD_Connection *conn, int status, const char *body)
+struct http_response *
+http_mkresp(struct MHD_Connection *conn, int status, const char *body)
 {
 	struct http_response *resp;
 
@@ -181,199 +188,201 @@ struct http_response *http_mkresp(struct MHD_Connection *conn, int status, const
 	return (resp);
 }
 
-int send_response(struct http_response *resp)
+int
+send_response(struct http_response *resp)
 {
-	int ret;
-	struct MHD_Response *MHDresponse;
-	void *data_copy;
+	struct MHD_Response *response;
 	struct http_header *hdr;
-	data_copy = malloc(resp->ndata);
-	memcpy(data_copy, resp->data, resp->ndata);
-	MHDresponse = MHD_create_response_from_data(resp->ndata,
-			data_copy, MHD_YES, MHD_NO);
-	assert(MHDresponse);
-	for (hdr = resp->headers; hdr; hdr = hdr->next)
-		MHD_add_response_header(MHDresponse, hdr->key, hdr->value);
+	void *data;
+	int ret;
 
-	ret = MHD_queue_response (resp->connection, resp->status, MHDresponse);
-	MHD_destroy_response (MHDresponse);
-	return ret;
+	data = malloc(resp->ndata);
+	memcpy(data, resp->data, resp->ndata);
+	response = MHD_create_response_from_data(resp->ndata, data,
+	    MHD_YES, MHD_NO);
+	assert(response);
+	for (hdr = resp->headers; hdr; hdr = hdr->next)
+		MHD_add_response_header(response, hdr->key, hdr->value);
+	ret = MHD_queue_response(resp->connection, resp->status, response);
+	MHD_destroy_response(response);
+	return (ret);
 }
 
-static void request_completed (void *cls, struct MHD_Connection *connection,
-		   void **con_cls, enum MHD_RequestTerminationCode toe)
+static void
+request_completed(void *cls, struct MHD_Connection *connection,
+    void **con_cls, enum MHD_RequestTerminationCode code)
 {
+	struct connection_info_struct *con_info =
+	    (struct connection_info_struct *)*con_cls;
+
 	(void)cls;
 	(void)connection;
-	(void)toe;
-	struct connection_info_struct *con_info =
-		(struct connection_info_struct *) *con_cls;
+	(void)code;
 
-
-	if (!con_info)
-		return;
-
-	free(con_info);
-	*con_cls = NULL;
+	if (con_info) {
+		free(con_info);
+		*con_cls = NULL;
+	}
 }
 
-static int find_listener(struct http_request *request, struct http_priv_t *http)
+static int
+find_listener(struct http_request *request, struct http_priv_t *http)
 {
-	struct http_listener *listener;
+	struct http_listener *lp;
+
 	assert(request);
-	for (listener = http->listener; listener != NULL; listener = listener->next) {
-		if (!strncmp(listener->url,request->url,strlen(listener->url))) {
-			if (!(listener->method & request->method))
-				return 0;
-			listener->cb(request, listener->data);
-			return 1;
+	for (lp = http->listener; lp != NULL; lp = lp->next) {
+		if (!strncmp(lp->url, request->url, strlen(lp->url))) {
+			if (!(lp->method & request->method))
+				return (0);
+			lp->cb(request, lp->data);
+			return (1);
 		}
 	}
-	return 0;
+	return (0);
 }
 
-struct header_finder_t {
-	const char *header;
-	char *value;
-};
-
-static int get_key (void *cls, enum MHD_ValueKind kind, const char *key, const char *value)
+static int
+get_key(void *cls, enum MHD_ValueKind kind, const char *key,
+    const char *value)
 {
 	struct header_finder_t *finder = cls;
+
 	(void)kind;
+
 	if (!strcasecmp(finder->header, key)) {
 		finder->value = strdup(value);
-		return MHD_NO;
+		return (MHD_NO);
 	}
-	return MHD_YES;
+	return (MHD_YES);
 }
 
-static char *http_get_header(struct MHD_Connection *connection, const char *header)
+static char *
+http_get_header(struct MHD_Connection *connection, const char *header)
 {
 	struct header_finder_t finder;
+
 	finder.header = header;
 	finder.value = NULL;
-	MHD_get_connection_values (connection, MHD_HEADER_KIND, &get_key, &finder);
-	return finder.value;
+	MHD_get_connection_values(connection, MHD_HEADER_KIND, &get_key,
+	    &finder);
+	return (finder.value);
 }
 
-static void log_request(struct MHD_Connection *connection,
-			const struct http_priv_t *http,
-			const char *method,
-			const char *url)
+static void
+log_request(struct MHD_Connection *connection,
+    const struct http_priv_t *http, const char *method, const char *url)
 {
-
-
 #if MHD_VERSION < 0x00090600
 	const union MHD_ConnectionInfo *info;
 	const unsigned char *ip;
-	info = MHD_get_connection_info (connection,
-			MHD_CONNECTION_INFO_CLIENT_ADDRESS);
+
+	info = MHD_get_connection_info(connection,
+	    MHD_CONNECTION_INFO_CLIENT_ADDRESS);
 	assert(info);
 
 	ip = (unsigned char *)&info->client_addr->sin_addr.s_addr;
 	logger(http->logger, "%hhu.%hhu.%hhu.%hhu:%d - %s %s",
-	       ip[0], ip[1], ip[2], ip[3], info->client_addr->sin_port,
-	       method, url);
+	    ip[0], ip[1], ip[2], ip[3], info->client_addr->sin_port,
+	    method, url);
 #else
 	(void)connection;
+
 	logger(http->logger, "%s %s", method, url);
 #endif
 }
 
-static int check_auth(struct MHD_Connection *connection, struct agent_core_t *core, struct connection_info_struct *con_info)
+static int
+check_auth(struct MHD_Connection *connection, struct agent_core_t *core,
+    struct connection_info_struct *con_info)
 {
+	char passwd[1024];
 	char *auth;
-	char base64pw[1024];
+
 	assert(con_info);
 	if (con_info->authed == 0) {
 		auth = http_get_header(connection, "Authorization");
 		/*
 		 * XXX: Should be stored somewhere...
 		 */
-		base64_encode(BASE64, core->config->userpass, strlen(core->config->userpass), base64pw, sizeof(base64pw));
-		if (!auth || strncmp(auth,"Basic ", strlen("Basic ")) || strcmp(auth + strlen("Basic "), base64pw)) {
+		base64_encode(BASE64, core->config->userpass,
+		    strlen(core->config->userpass), passwd,
+		    sizeof(passwd));
+		if (!auth || strncmp(auth, "Basic ", strlen("Basic ")) ||
+		    strcmp(auth + strlen("Basic "), passwd)) {
 			send_auth_response(connection);
 			free(auth);
-			return 1;
+			return (1);
 		}
 		free(auth);
 		con_info->authed = 1;
 	}
-	return 0;
+	return (0);
 }
 
-static int answer_to_connection (void *cls, struct MHD_Connection *connection,
-                      const char *url, const char *method,
-                      const char *version, const char *upload_data,
-                      size_t *upload_data_size, void **con_cls)
+static int
+answer_to_connection(void *cls, struct MHD_Connection *connection,
+    const char *url, const char *method,
+    const char *version, const char *upload_data,
+    size_t * upload_data_size, void **con_cls)
 {
 	struct agent_core_t *core = (struct agent_core_t *)cls;
 	struct http_priv_t *http;
 	struct agent_plugin_t *plug;
 	struct http_request request;
-	struct connection_info_struct *con_info = NULL;
-	int ret;
+	struct connection_info_struct *con_info;
 
 	(void)version;
-	plug = plugin_find(core,"http");
-	http = (struct http_priv_t *) plug->data;
+
+	plug = plugin_find(core, "http");
+	http = (struct http_priv_t *)plug->data;
 	assert(http);
 
-	if (NULL == *con_cls) {
-		con_info = malloc (sizeof (struct connection_info_struct));
-		assert(con_info);
-		con_info->answerstring[0] = '\0';
-		con_info->progress = 0;
-		con_info->authed = 0;
+	if (*con_cls == NULL) {
+		ALLOC_OBJ(con_info);
 		*con_cls = con_info;
-		return MHD_YES;
+		return (MHD_YES);
 	}
 	con_info = *con_cls;
 	assert(core->config->userpass);
 
 	log_request(connection, http, method, url);
 
-	if (0 == strcmp (method, "GET") || !strcmp(method, "HEAD") || !strcmp(method,"DELETE")) {
-		ret = check_auth(connection, core, con_info);
-		if (ret == 1)
-			return MHD_YES;
-		if (!strcmp(method,"DELETE")) {
+	if (!strcmp(method, "GET") || !strcmp(method, "HEAD") ||
+	    !strcmp(method, "DELETE")) {
+		if (check_auth(connection, core, con_info))
+			return (MHD_YES);
+		if (!strcmp(method, "DELETE"))
 			request.method = M_DELETE;
-		} else {
+		else
 			request.method = M_GET;
-		}
 		request.connection = connection;
 		request.url = url;
 		request.ndata = 0;
 		if (find_listener(&request, http))
-			return MHD_YES;
-	}
-
-
-	if (!strcmp(method, "POST") || !strcmp(method, "PUT")) {
-
+			return (MHD_YES);
+	} else if (!strcmp(method, "POST") || !strcmp(method, "PUT")) {
 		if (*upload_data_size != 0) {
-			if (*upload_data_size + con_info->progress >= RCV_BUFFER) {
-				warnlog(http->logger, "Client input exceeded buffer size of %u bytes. Dropping client.", RCV_BUFFER);
-
-				 return MHD_NO;
+			if (*upload_data_size + con_info->progress >=
+			    RCV_BUFFER) {
+				warnlog(http->logger,
+				    "Client input exceeded buffer size "
+				    "of %u bytes. Dropping client.",
+				    RCV_BUFFER);
+				return (MHD_NO);
 			}
 			memcpy(con_info->answerstring + con_info->progress,
-				upload_data, *upload_data_size);
+			    upload_data, *upload_data_size);
 			con_info->progress += *upload_data_size;
 			*upload_data_size = 0;
-
-			return MHD_YES;
-		} else if (NULL != con_info->answerstring){
-			ret = check_auth(connection, core, con_info);
-			if (ret == 1)
-				return MHD_YES;
-			if (!strcmp(method,"POST")) {
+			return (MHD_YES);
+		} else if ((char *)con_info->answerstring != NULL) {
+			if (check_auth(connection, core, con_info))
+				return (MHD_YES);
+			if (!strcmp(method, "POST"))
 				request.method = M_POST;
-			} else {
+			else
 				request.method = M_PUT;
-			}
 			request.connection = connection;
 			request.url = url;
 			request.ndata = con_info->progress;
@@ -383,41 +392,40 @@ static int answer_to_connection (void *cls, struct MHD_Connection *connection,
 			 */
 			((char *)request.data)[con_info->progress] = '\0';
 			if (find_listener(&request, http))
-				return MHD_YES;
+				return (MHD_YES);
 		}
 	}
 	if (request.method == M_GET && !strcmp(url, "/")) {
 		if (http->help_page == NULL)
 			http->help_page = make_help(http);
-		assert (http->help_page);
-		return send_response_ok(connection, http->help_page);
+		assert(http->help_page);
+		return (send_response_ok(connection, http->help_page));
 	}
 
-
-	return send_response_fail (connection, "Failed\n");
+	return (send_response_fail(connection, "Failed"));
 }
 
-static void *http_run(void *data)
+static void *
+http_run(void *data)
 {
 	struct agent_core_t *core = (struct agent_core_t *)data;
 	struct agent_plugin_t *plug;
 	struct http_priv_t *http;
-	int port;
 	struct MHD_Daemon *d;
-	assert(data);
-	assert(core);
-	plug = plugin_find(core,"http");
-	http = (struct http_priv_t *) plug->data;
+	int port;
+
+	plug = plugin_find(core, "http");
+	http = (struct http_priv_t *)plug->data;
 	assert(http);
 	port = atoi(core->config->c_arg);
 	assert(port > 0);
-	logger(http->logger, "HTTPd starting on port %i",port);
-	d = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, port, NULL, NULL,
-                             &answer_to_connection, data,
-                             MHD_OPTION_NOTIFY_COMPLETED, request_completed,
-                             NULL, MHD_OPTION_END);
+	logger(http->logger, "HTTP starting on port %i", port);
+	d = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, port, NULL, NULL,
+	    &answer_to_connection, data, MHD_OPTION_NOTIFY_COMPLETED,
+	    request_completed, NULL, MHD_OPTION_END);
 	if (!d) {
-		warnlog(http->logger, "HTTP failed to start on port %d. Agent already running?",port);
+		warnlog(http->logger, "HTTP failed to start on port %i. "
+		    "Agent already running?", port);
 		sleep(1);
 		exit(1);
 	}
@@ -425,36 +433,34 @@ static void *http_run(void *data)
 	/*
 	 * XXX: .....
 	 */
-	while (1)
+	for (;;)
 		sleep(100);
 	MHD_stop_daemon(d);
-
-	return NULL;
+	return (NULL);
 }
 
-
-int http_register_url(struct agent_core_t *core, const char *url,
-		       unsigned int method,
-		       callback_t cb,
-		       void *data)
+int
+http_register_url(struct agent_core_t *core, const char *url,
+    unsigned int method, callback_t cb, void *data)
 {
-	struct http_listener *listener;
+	struct http_listener *lp;
 	struct agent_plugin_t *plug;
 	struct http_priv_t *http;
 
-	ALLOC_OBJ(listener);
-	plug = plugin_find(core,"http");
+	assert(cb);
+
+	ALLOC_OBJ(lp);
+	plug = plugin_find(core, "http");
 	http = plug->data;
 	assert(http);
-	listener->url = strdup(url);
-	assert(listener->url);
-	assert(cb);
-	listener->method = method;
-	listener->cb = cb;
-	listener->data = data;
-	listener->next = http->listener;
-	http->listener = listener;
-	return 1;
+	lp->url = strdup(url);
+	assert(lp->url);
+	lp->method = method;
+	lp->cb = cb;
+	lp->data = data;
+	lp->next = http->listener;
+	http->listener = lp;
+	return (1);
 }
 
 void
@@ -494,8 +500,8 @@ http_init(struct agent_core_t *core)
 	struct http_priv_t *priv;
 
 	ALLOC_OBJ(priv);
-	plug = plugin_find(core,"http");
-	priv->logger = ipc_register(core,"logger");
+	plug = plugin_find(core, "http");
+	priv->logger = ipc_register(core, "logger");
 	plug->data = (void *)priv;
 	plug->start = http_start;
 }
