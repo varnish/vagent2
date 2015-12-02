cc0c76f985b91efcba2adce530ffb94137abd0d9
Kristian Lyngstol <kristian@bohemians.org>
2015-12-02 13:53:42 +0100

    Add some better argument-checking

--- tests/arguments.sh
+++ tests/arguments.sh
@@ -3,6 +3,7 @@
 . util.sh
 
 ARGS=$(grep '= getopt(' $ORIGPWD/../src/main.c | sed -e 's/[^"]*"//; s/"[^"]*$//; s/://g; s/\(.\)/\1 /g' )
+ARGS2=$(awk '/^OPTIONS/ { opt=1 }; opt==1 && /^\-/ {print $1}; /^VARNISH/ {opt=0}' $ORIGPWD/../README.rst)
 
 for a in $ARGS; do
 	$ORIGPWD/../src/varnish-agent -h  2>&1 | egrep -q "^[ ]*-$a";
@@ -12,13 +13,36 @@ for a in $ARGS; do
 	inc
 done
 
-
 for a in $ARGS; do
 	egrep -q "^-$a" $ORIGPWD/../README.rst;
 	if [ $? -eq "0" ]; then pass;
 	else fail "$a missing in README.rst"
 	fi
 	inc
+done
+
+for a in $ARGS2; do
+	$ORIGPWD/../src/varnish-agent -h  2>&1 | egrep -q "^[ ]*$a";
+	if [ $? -eq "0" ]; then pass;
+	else fail "$a missing in -h"
+	fi
+	inc
+done
+
+for a in $ARGS2; do
+	b=$(echo -- $a | sed 's/-//g; s/ //g;')
+	echo $ARGS | grep -q $b
+	if [ $? -eq "0" ]; then pass;
+	else fail "$a missing in getopt()"
+	fi
+	inc
+
+	$ORIGPWD/../src/varnish-agent -h  2>&1 | egrep -q "^[ ]*-$b";
+	if [ $? -eq "0" ]; then pass;
+	else fail "$b missing in -h"
+	fi
+	inc
+
 done
 
 exit $ret
