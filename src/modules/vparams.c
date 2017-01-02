dc2669f5434626a4a5951c2ec3c1249351fb5d05
Guillaume Quintard <guillaume@varnish-software.com>
2017-01-02 12:13:45 +0100

    Register urls without trailing slash

--- src/modules/vparams.c
+++ src/modules/vparams.c
@@ -451,8 +451,8 @@ vparams_init(struct agent_core_t *core)
 	priv->logger = ipc_register(core,"logger");
 	priv->vadmin = ipc_register(core,"vadmin");
 	plug->data = (void *)priv;
-	http_register_path(core, "/param/", M_PUT | M_GET, vparams_reply, core);
-	http_register_path(core, "/paramjson/", M_GET, vparams_json_reply,
+	http_register_path(core, "/param", M_PUT | M_GET, vparams_reply, core);
+	http_register_path(core, "/paramjson", M_GET, vparams_json_reply,
 			core);
 	http_register_path(core, "/help/param", M_GET, help_reply, strdup(PARAM_HELP));
 }
